4f298e10282f896fb6a7328ba76f358692b1eb19
Yemrekayaa <yemrekayatr@gmail.com>
2024-08-30 19:34:40 +0300

    Remove docker files and edit Program.cs

--- WebAPI/Controllers/PlayerController.cs
+++ WebAPI/Controllers/PlayerController.cs
@@ -6,7 +6,7 @@ using SHAnalytics.Application.Features.Players.Queries.GetList;
 
 namespace WebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/players")]
     [ApiController]
     public class PlayerController : ControllerBase
     {
