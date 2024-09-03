5b347228071ca39ed0347d5b06f4cafb08a39e4c
Yemrekayaa <yemrekayatr@gmail.com>
2024-09-04 01:56:25 +0300

    Edit Get Selected Card Options Count

--- SHAnalytics.Application/Features/CardOptions/Queries/GetSelectedCount/GetSelectedStatsCardOptionQuery.cs
+++ SHAnalytics.Application/Features/CardOptions/Queries/GetSelectedCount/GetSelectedStatsCardOptionQuery.cs
@@ -40,14 +40,19 @@ namespace SHAnalytics.Application.Features.CardOptions.Queries.GetSelectedCount
                         .Select(g => new GetSelectedStatsCardOptionResponse
                         {
                             Name = g.Key,
+                            Temp = g.Count(co => !co.IsPerma),
                             Total = g.Count(),
                             Perma = g.Count(co => co.IsPerma),
-                            Selected = g.Count(co => co.IsSelected),
+                            TotalSelected = g.Count(co => co.IsSelected),
                             PermaSelected = g.Count(co => co.IsSelected && co.IsPerma),
                             TempSelected = g.Count(co => co.IsSelected && !co.IsPerma),
                             TotalSelectionRate = (int)Math.Floor((g.Count(co => co.IsSelected) / (double)g.Count()) * 100),
-                            PermaSelectionRate = (int)Math.Floor((g.Count(co => co.IsSelected && co.IsPerma) / (double)g.Count()) * 100),
-                            TempSelectionRate = (int)Math.Floor((g.Count(co => co.IsSelected && !co.IsPerma) / (double)g.Count()) * 100)
+                            PermaSelectionRate = g.Count(co => co.IsPerma) > 0
+                            ? (int)Math.Floor((g.Count(co => co.IsSelected && co.IsPerma) / (double)g.Count(co => co.IsPerma)) * 100)
+                            : 0,
+                            TempSelectionRate = g.Count(co => !co.IsPerma) > 0
+                            ? (int)Math.Floor((g.Count(co => co.IsSelected && !co.IsPerma) / (double)g.Count(co => !co.IsPerma)) * 100)
+                            : 0,
                         }).ToList();
 
                 return response;
--- SHAnalytics.Application/Features/CardOptions/Queries/GetSelectedCount/GetSelectedStatsCardOptionResponse.cs
+++ SHAnalytics.Application/Features/CardOptions/Queries/GetSelectedCount/GetSelectedStatsCardOptionResponse.cs
@@ -4,12 +4,14 @@ namespace SHAnalytics.Application.Features.CardOptions.Queries.GetSelectedCount
     {
         public string Name { get; set; }
         public int Total { get; set; }
-        public int Selected { get; set; }
+        public int TotalSelected { get; set; }
+        public int TotalSelectionRate { get; set; }
+        public int Temp { get; set; }
+        public int TempSelected { get; set; }
+        public int TempSelectionRate { get; set; }
         public int Perma { get; set; }
         public int PermaSelected { get; set; }
-        public int TempSelected { get; set; }
         public int PermaSelectionRate { get; set; }
-        public int TempSelectionRate { get; set; }
-        public int TotalSelectionRate { get; set; }
+
     }
 }
